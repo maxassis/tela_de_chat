a21e4bb9fb666f0614317b12b202553d97c94a1d
max <max.assis@chatguru.com.br>
2023-06-12 14:48:28 -0300

    feat: iniciando o projeto

---
+++ app.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <div class="external-container">
+      <Header />
+
+      
+        <div class="chat-list">
+          <ChatsList />
+        </div>
+        <div class="messages">messages</div>
+        <div class="menu">menu</div> 
+       
+    
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.external-container {
+  display: grid;
+  grid-template-rows: 3.313rem calc(100dvh - 3.313rem);
+  grid-template-columns: 19.813rem minmax(39.375rem, 66.313rem) 3.875rem;
+  inline-size: 100%;
+  block-size: 100%;
+  // border: 1px solid red;
+
+ 
+
+}
+
+.header {
+  grid-column: 1/4;
+  grid-row: 1 / 2;
+ // border: 1px solid red;
+}
+
+.chat-list {
+  grid-column: 1/2;
+  grid-row: 2 / 3;
+}
+
+.messages {
+  grid-column: 2/3;
+  grid-row: 2 / 3;
+}
+
+.menu {
+  grid-column: 3/4;
+  grid-row: 2 / 3;
+}
+</style>
+
+<script setup lang="ts">
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;900&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
+
---
+++ components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="header">
+        <div class="header__user-wrapper">
+            <img src="../public/images/user.png" alt="user" />
+
+            <div class="header__name-wrapper">
+                <h3>Roberta da Silva</h3>
+                <span>Tech Lead</span>
+            </div>
+
+        </div>
+
+        <div class="header__menu red"></div>
+
+
+
+    </div>
+
+
+</template>
+
+
+
+<style scoped lang="scss">
+    .header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        inline-size: 100%;
+        block-size: 100%;
+        background: #333333;
+        padding: 0 1.188rem 0 1rem;
+
+        &__user-wrapper {
+            display: flex;
+            align-items: center;
+            height: 2.313rem;
+            gap: 16px;
+        }
+
+        &__menu {
+            inline-size: 20.188rem;
+            block-size: 1.813rem;
+        }
+
+        &__name-wrapper {
+            display: flex;
+            justify-content: space-between;
+            flex-direction: column;
+            gap: 2px;
+            block-size: 31px;
+
+            >span {
+                font-family: 'Roboto';
+                font-weight: 400;
+                font-size: 10px;
+                line-height: 12px;
+                color: #FFFFFF;
+                line-height: 9.1094px;
+            }
+
+            >h3 {
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 19px;
+                color: #FFFFFF;
+                line-height: 13.375px;
+            }
+        }
+
+
+
+    }
+
+</style>
